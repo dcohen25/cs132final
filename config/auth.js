bda9c8bd28ec17eb30ccd876ec3c7a2ea8a1c27d
Sami Baghdady <Sami@Samis-MacBook-Air.local>
2016-05-18 17:36:54 -0400

    Facebook auth fixed

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@
  	'facebookAuth' : {
  		'clientID' : '813658328765982',
  		'clientSecret' : 'e602bf0474b7228a446062f74eba75e0',
- 		'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+ 		'callbackURL' : 'http://ancient-stream-38621.herokuapp.com/auth/facebook/callback'
  	}
  };
